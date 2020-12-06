016d06a113f65252a3d0966a364b5e6bfb167631
lenz <lenz1986@gmail.com>
2020-12-06 11:51:19 +0000

    fix target packages

--- files/etc/domotics.sh
+++ files/etc/domotics.sh
@@ -10,9 +10,6 @@ wget https://github.com/lmahmutov/lumi_packages/raw/main/shadow-usermod_4.8.1-1_
 
 echo "start installation"
 opkg update
-opkg install git
-opkg install git-http
-opkg install curl
 opkg install /tmp/liblua5.3-5.3_5.3.5-4_arm_cortex-a9_neon.ipk
 opkg install /tmp/lua5.3_5.3.5-4_arm_cortex-a9_neon.ipk
 opkg install /tmp/domoticz_2020.2-3_arm_cortex-a9_neon.ipk
